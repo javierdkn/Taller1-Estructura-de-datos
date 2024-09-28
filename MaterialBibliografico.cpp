0d6caf47db7e9cfcf9607e11b9c40942feec9199
javierdkn <javierdinamarcafredes@gmail.com>
2024-09-28 18:50:41 -0300

    add archive MaterialBibliografico.cpp

---
+++ MaterialBibliografico.cpp
@@ -0,0 +1,37 @@
+#include "MaterialBibliografico.h"
+
+
+MaterialBibliografico::MaterialBibliografico(string nombre ,string isbn, string autor){
+     this-> nombre = nombre;
+     this-> isbn = isbn;
+     this-> autor = autor;
+     this-> prestado = false;
+
+}
+
+string MaterialBibliografico::getNombre() {
+    return this-> nombre;
+}
+string MaterialBibliografico::getIsbn() {
+    return this-> isbn;
+}
+string MaterialBibliografico::getAutor() {
+    return this-> autor;
+}
+bool MaterialBibliografico::getPrestado() {
+    return this-> prestado;
+}
+
+void MaterialBibliografico::setPrestado(bool estado) {
+    prestado = estado;
+}
+
+MaterialBibliografico::~MaterialBibliografico(){}
+
+
+void MaterialBibliografico::mostrarInformacion(){
+    cout << "nombre: " << nombre << endl;
+    cout << "isbn: " << isbn << endl;
+    cout << "prestado: " << prestado << endl;
+
+}
